8ddf347ce9a743ed0309556050db3f4a70b29597
Swapnil Sonawane <swapnilsonawane.svpm@gmail.com>
2024-06-26 22:36:32 -0700

    json

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const githubData = {
+  name: 'Swapnil',
+  age: 22,
+  city: 'Indore'
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/name',(req,res) => {
+  res.send('Swapnil')
+})
+
+app.get('/github',(req,res) => {
+  res.send(githubData)
 })
 
 app.listen(process.env.PORT, () => {
